be6dfc8d266dede33c9f414b2e897d073f2fb0d9
Alexander Ananchenko <snvtr@mail.ru>
2019-04-02 15:41:52 +0300

    sqlite example

--- base/sqlite.py
+++ base/sqlite.py
@@ -6,30 +6,30 @@ script, filename = sys.argv
 try:
     conn = sqlite3.connect('c:/temp/main.db')
     recordset = conn.cursor()
-    sqlcmd = "select max(id) from bats_users"
+    sqlcmd = 'select max(id) from bats_users'
     recordset.execute(sqlcmd)
 
 except Exception:
-    print "database error. quitting (-1)."
+    print('database error. quitting (-1).')
     sys.exit(-1)
 
 fields = recordset.fetchone()
 max_id = int(fields[0])
 
 try:
-    f = open(filename,"r")
+    f = open(filename,'r')
 
 except Exception:
-    print "cannot open file for reading. quitting (-2)."
+    print('cannot open file for reading. quitting (-2).')
     sys.exit(-2)
 
 for line in f:
-    fields = line.split("\t")
+    fields = line.split('\t')
     user = fields[1]
     login = fields[2]
     logout = fields[3]
     max_id += 1
-    sqlcmd = "insert into bats_users values(%d,'%s','%s','%s')" % (max_id, user, login, logout)
+    sqlcmd = 'insert into bats_users values(%d,'%s','%s','%s')' % (max_id, user, login, logout)
     recordset.execute(sqlcmd)
 
 f.close()
