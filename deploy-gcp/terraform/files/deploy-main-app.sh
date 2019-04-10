fb8ddec9950224ee3ddcc22e46f477f55f0c1bfe
Alexander Ananchenko <snvtr@mail.ru>
2019-04-10 14:42:08 +0300

    Update deploy-main-app.sh

--- deploy-gcp/terraform/files/deploy-main-app.sh
+++ deploy-gcp/terraform/files/deploy-main-app.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# нерабочая фича. надо ставить сервисом и запускать сервисом
-
 # just in case:
 echo export JSON_SRV_HOST=10.132.0.5 > /etc/profile
 echo export JSON_SRV_PORT=7070 > /etc/profile
